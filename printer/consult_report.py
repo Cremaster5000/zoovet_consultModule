e67ca01828edebee35724916e6974e0b76d95b84
Eduardo Tellez <tellezlalo94@gmail.com>
2024-05-21 14:29:15 -0600

    First commit, it creates a main window where you can put the animal's information and generate a new consult, save info, print recipe and consult report. It can calculate some prices for nex visit depending on species and weight.

--- printer/consult_report.py
+++ printer/consult_report.py
@@ -8,23 +8,14 @@ class Consult_report():
     def __init__(self, filename):
         self.file = filename
         
-    def printReport(self):
-        if os.path.exists(f"{self.file}.html"):
-            self.htmlToPDF()
-            self.openToPrint()
-        self.getData()
-        self.createHtml()
-        self.htmlToPDF()
-        self.openToPrint()
-        
     def getData(self):
         try:
             with open(f"printer/consults/{self.file}.json", "r") as consult:
                 data = json.loads(consult.read())
             self.date = self.transformDate(data["date"][0:-6])
-            self.type = self.setTypeConsult(data["type"]),
+            self.type = data["type"]
             self.anamnesis = data["anamnesis"]
-            self.name = data["name"]
+            self.patient = data["patient"]
             self.edo_mental = data["mental"]
             self.linfo = data["linfonode"]
             self.fc = data["fc"]
@@ -35,15 +26,22 @@ class Consult_report():
             self.weight = data["weight"]
             self.rd = data["rd"]
             self.apulmo = data["auscPulm"]
+            self.acard = data["auscCardiac"]
             self.pabd = data["abdPalpat"]
             self.history = data["clinicHistory"]
             self.notes = data["notes"]
             self.next = self.transformDate(data["nextVisit"])
             self.motive = data["motive"]
-            self.cost = self.findWords()
+            self.cost = data["cost"]
         except Exception as e:
             print("Error: ", e)
             
+    def transformDate(self, date):
+        day, month, year = date.split("-")
+        month = self.translate(month)
+        date = f"{day} de {month} de {year}"
+        return date
+    
     def translate(self, month):
         spanish = {
                     "01": "enero",
@@ -60,66 +58,22 @@ class Consult_report():
                     "12": "diciembre"
                  }
         return spanish[month]
-
-    def transformDate(self, date):
-        day, month, year = date.split("-")
-        month = self.translate(month)
-        date = f"{day} de {month} de {year}"
-        return date
-    
-    def setTypeConsult(self, type):
-        types = {
-                "Consulta general":0,
-                "Cita control":1,
-                "Entrega de resultados":2,
-                "Otro":3
-        }
-        return types[type]
-    
-    def findWords(self):
-        price = ""
-        control_cite = ["cita control", "control", "seguimiento"]
-        general_cite = ["consulta general", "primera consulta", "primera visita", "consulta"]
-        desparasitation_cite = ["desparasitación interna", "desparasitante", "desparasitación", "desparasitacion"]
-        laboratories = ["hemograma", "biometria hematica"]
-        print("entered in findwords")
-        try:
-            for word in self.motive.split("+"):
-                word = word.rstrip().lstrip()        
-                print(self.motive)
-                if price != "": price+=(" + ")
-                if word in control_cite: price+=("$300")
-                if word in general_cite: price+=("$380")
-                if word in desparasitation_cite: price += self.desparasitation_price()
-                if word in laboratories: price+=("$400")
-            price += "."
-        except Exception as e:
-            print("Error:", e)
-        return price 
-    
-    def desparasitation_price(self):
-        if self.species == "Felino":
-            return "($154)"
-        weight = float(self.weight.replace("kg", "").replace(" ",""))
-        if weight < 5:
-            return "($155)"
-        if weight >= 5 and weight <10:
-            return "($188)"
-        if weight >= 10 and weight <15:
-            return "($220)"
-        if weight >= 15 and weight <20:
-            return "($252.5)"
-        if weight >= 20 and weight <30:
-            return "($253)"
-        if weight >= 30 and weight <40:
-            return "($318)" 
-                    
+        
+    def printReport(self):
+        if os.path.exists(f"{self.file}.html"):
+            self.htmlToPDF()
+            self.openToPrint()
+        self.getData()
+        self.createHtml()
+        self.htmlToPDF()
+        self.openToPrint()
+ 
     def createHtml(self):
         print("entered to create report html")
         environment = Environment(loader=FileSystemLoader("printer/templates/"))
         template = environment.get_template("template_consult_report.html")        
-        content = template.render(date = self.date, patient = self.name, anamnesis = self.anamnesis, tc = self.tc, cc = self.cc,
-                                  emental = self.edo_mental, notes = self.notes, next = self.next, linfo = self.linfo, 
+        content = template.render(date = self.date, patient = self.patient, anamnesis = self.anamnesis, tc = self.tc, cc = self.cc,
+                                  emental = self.edo_mental, notes = self.notes, next = self.next, linfo = self.linfo, acard = self.acard,
                                   type = self.type[0], motive = self.motive, rt = self.rt, fc = self.fc, weight = self.weight,
                                   fr = self.fr, rd = self.rd, apulmo = self.apulmo, pabd = self.pabd, cost = self.cost,
                                   history = self.history)
--- printer/recipe.py
+++ printer/recipe.py
@@ -8,7 +8,31 @@ import subprocess
 class Recipe():
     def __init__(self, filename):
         self.file = filename
-        
+
+    def getData(self):
+        try:
+            with open(f"printer/consults/{self.file}.json", "r") as consult:
+                data = json.loads(consult.read())
+            self.date = self.transformDate(data["date"][0:-6])
+            self.type = data["type"]
+            self.anamnesis = data["anamnesis"]
+            self.patient = data["patient"]
+            self.owner = data["owner"]                        
+            self.weight = data["weight"]
+            self.dx = data["dx"]
+            self.tx = data["tx"]
+            self.next = self.transformDate(data["nextVisit"])
+            self.motive = data["motive"]
+            self.cost = data["cost"]
+        except Exception as e:
+            print("Error: ", e)
+            
+    def transformDate(self, date):
+        day, month, year = date.split("-")
+        month = self.translate(month)
+        date = f"{day} de {month} de {year}"
+        return date
+    
     def translate(self, month):
         spanish = {
                     "01": "enero",
@@ -25,34 +49,11 @@ class Recipe():
                     "12": "diciembre"
                  }
         return spanish[month]
-
-    def transformDate(self, date):
-        day, month, year = date.split("-")
-        month = self.translate(month)
-        date = f"{day} de {month} de {year}"
-        return date
-
-    def getData(self):
-        try:
-            print(os.path.relpath(f"consults/{self.file}.json"))
-            with open(f"printer/consults/{self.file}.json", "r") as consult:
-                raw_data = json.loads(consult.read())
-            self.date = self.transformDate(raw_data["date"][0:-6])
-            self.name = raw_data["name"]
-            self.owner = raw_data["owner"]
-            self.weight = raw_data["weight"]
-            self.dx = raw_data["dx"]
-            self.tx = raw_data["tx"].replace("\n", "<br>")
-            self.next = self.transformDate(raw_data["nextVisit"])
-            self.motive = raw_data["motive"]
-            self.cost = self.findWords(self.motive)
-        except Exception as e:
-            print("Error: ", e)
-
+    
     def createHtml(self):
         environment = Environment(loader=FileSystemLoader("printer/templates/"))
         template = environment.get_template("template_recipe.html")        
-        content = template.render(date = self.date, patient = self.name,
+        content = template.render(date = self.date, patient = self.patient,
                                   propietary = self.owner, weight = self.weight,
                                   dx = self.dx, tx = self.tx, next_visit = self.next,
                                   motive = self.motive, cost = self.cost)
@@ -69,22 +70,7 @@ class Recipe():
     def openToPrint(self):
         path = os.path.abspath(f"printer/pdf/{self.file}.pdf")
         subprocess.run(["evince", path])
-        
-    def findWords(self, motive):
-        price = ""
-        control_cite = ["cita control", "control", "seguimiento"]
-        general_cite = ["consulta general", "primera consulta", "primera visita", "consulta"]
-        desparasitation_cite = ["desparasitación interna", "desparasitante", "desparasitación", "desparasitacion"]
-        laboratories = ["hemograma", "biometria hematica"]
-        for word in motive.split("+"):
-            word = word.rstrip().lstrip()
-            if price != "": price+=(" + ")
-            if word in control_cite: price+=(f"{word} ($300)")
-            if word in general_cite: price+=(f"{word} ($380)")
-            if word in desparasitation_cite: price+=(f"{word} ($180)")
-            if word in laboratories: price+=(f"{word} ($400)")
-        return price  
-            
+
     def printRecipe(self):
         self.getData()
         self.createHtml()
